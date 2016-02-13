e9c447cf554259c874280feae56bbb52358f0314
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-02-13 16:57:57 -0200

    Added Preferences to user successfully

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_action :set_collections, only: [:new, :create, :edit, :update]
+  before_action :set_collections, only: [:create]
   private
   def set_collections
-    @preferences = Preference.all
+    params[:user][:cuisines] = params[:user][:cuisines].join(', ')
   end
 
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :city, :facebook,
-                                 :twitter, :email, :password,
+                                 :twitter, :email, :password, :cuisines,
                                  :password_confirmation)
   end
 
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :city, :facebook,
-                                 :twitter, :email, :password,
+                                 :twitter, :email, :password, :cuisines,
                                  :password_confirmation, :current_password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213111509) do
+ActiveRecord::Schema.define(version: 20160213154051) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20160213111509) do
     t.string   "city"
     t.string   "facebook"
     t.string   "twitter"
+    t.string   "cuisines"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
