0228e83880912d32e5a87ab903f6b4a06b37b10a
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-02-16 23:18:14 -0200

    i18n configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
-  root 'recipes#index'
-  resources :users, only:[:show]
-  resources :recipes
-  resources :cuisines
-  resources :courses
-  resources :preferences
-  resources :favorites
+  scope "(:locale)", locale: /en|pt/ do
+    devise_for :users, :controllers => {registrations: 'registrations'}
+    root 'recipes#index'
+    get '/:locale' => 'recipes#index'
+    resources :users, only:[:show]
+    resources :recipes
+    resources :cuisines, only:[:new, :create, :show]
+    resources :courses, only:[:new, :create, :show]
+    resources :preferences, only:[:new, :create, :show]
+    resources :favorites, only:[:new, :create, :destroy]
+  end
 end
