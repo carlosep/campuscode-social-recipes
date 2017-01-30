422bf549129f5e646a3be22d63eb59845d48ab65
Carlos <carlao_epalha@hotmail.com>
2017-01-29 23:13:45 -0200

    Added test for unsuccessfull recipe editing

--- spec/features/user/user_edits_recipes_spec.rb
+++ spec/features/user/user_edits_recipes_spec.rb
@@ -27,6 +27,33 @@ feature 'User edits a recipe' do
     expect(page).to have_content 'mix it up and throw it away.'
   end
 
+  scenario 'unsuccessfully' do
+    user = create(:user)
+    recipe = create(:recipe, user: user)
+
+    user_sign_in(user: user, redirect: true)
+    visit edit_recipe_path(id: recipe)
+
+    fill_in 'Name',                with: ''
+    select recipe.cuisine.name,    from: 'Cuisine'
+    select recipe.course.name,     from: 'Course'
+    select recipe.preference.name, from: 'Preference'
+    select recipe.difficulty.name, from: 'Difficulty'
+    fill_in 'Portion',             with: ''
+    fill_in 'Cooking time',        with: ''
+    fill_in 'Ingredient',          with: ''
+    fill_in 'Directions',          with: ''
+
+    click_on 'Edit Recipe'
+
+    expect(page).to have_current_path("/en#{recipe_path(id: recipe)}")
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Portion can't be blank"
+    expect(page).to have_content "Cooking time can't be blank"
+    expect(page).to have_content "Ingredient can't be blank"
+    expect(page).to have_content "Directions can't be blank"
+  end
+
   scenario 'invalid editing of other users recipes' do
     other_user = create(:user)
     create(:recipe, user: other_user)
